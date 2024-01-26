925b660fefda3089ece345f1047bf23b9a4a417a
Chris Wilton-Magras <chriswilty@zoho.com>
2024-01-26 17:14:33 +0000

    Add API module - express server

---
+++ api/README.md
@@ -0,0 +1,13 @@
+# apigw-fargate-stacks : API
+
+A simple Node Express server, which can be run directly on command-line or in a Docker container.
+
+## Run me
+
+```
+# Locally
+yarn start
+
+# Watch mode
+yarn dev
+```
---
+++ api/server.ts
@@ -0,0 +1,81 @@
+import cors from 'cors';
+import express from 'express';
+import session from 'express-session';
+import memoryStoreFactory from 'memorystore';
+
+declare module 'express-session' {
+	interface SessionData {
+		count: number;
+	}
+}
+
+const allowOrigin = process.env['CORS_ALLOW_ORIGIN'];
+const counterPath = '/counter';
+const maxCookieAge = 60 * 60 * 1000; //1hr session keep-alive
+const port = 3000;
+const sessionSigningSecret =
+	'In a real app this might be read from a secrets manager';
+
+const app = express();
+const env = app.get('env');
+const isProd = env === 'production';
+console.log(`env=${env}`);
+
+// Generic middleware
+app
+	.use(express.json())
+	.use(cors({
+		origin: allowOrigin,
+		credentials: true,
+	}));
+
+// Session shenanigans
+app
+	.set('trust proxy', isProd)
+	.use(
+		counterPath,
+		session({
+			name: 'apigw-fargate-stacks.sid',
+			resave: false,
+			saveUninitialized: true,
+			secret: sessionSigningSecret,
+			store: new (memoryStoreFactory(session))({
+				checkPeriod: maxCookieAge,
+			}),
+			cookie: {
+				maxAge: maxCookieAge,
+				path: counterPath,
+				sameSite: isProd ? 'none' : 'strict',
+				secure: isProd
+			},
+		}),
+		(req, _res, next) => {
+			// initialise session first time
+			if (req.session.count === undefined) {
+				req.session.count = 0;
+			}
+			next();
+		}
+	);
+
+// Main route handlers
+app
+	.get(counterPath, (req, res) => {
+		res.send({ count: req.session.count });
+	})
+	.patch(`${counterPath}/up`, (req, res) => {
+		req.session.count = (req.session.count ?? 0) + 1;
+		res.send({ count: req.session.count });
+	})
+	.patch(`${counterPath}/down`, (req, res) => {
+		req.session.count = Math.max(0, (req.session.count ?? 0) - 1);
+		res.send({ count: req.session.count });
+	});
+
+// Health-check for remote deployment
+app.get('/health', (_req, res) => {
+	res.send();
+})
+
+// Run the bugger
+app.listen(port, () => console.log(`Server started on port ${port}`));
